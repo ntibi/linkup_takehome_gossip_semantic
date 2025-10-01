884016ec8563b8cf755a16650aafaacfa9d2ac09
ntibi <ntibi@protonmail.com>
2025-10-01 12:55:21 +0200

    added some tests

--- src/embed/tests/pooling.test.ts
+++ src/embed/tests/pooling.test.ts
@@ -1,4 +1,4 @@
-import { tokenize } from "../utils.js";
+import { chunkTokens, tokenize } from "../utils.js";
 
 describe('tokenize', () => {
 	it('splits by whitespace and removes empty tokens', () => {
@@ -7,3 +7,24 @@ describe('tokenize', () => {
 		expect(tokenize('hello  ')).toEqual(['hello']);
 	});
 });
+
+describe('chunkTokens', () => {
+	it('respects chunking/overlap rules', () => {
+		const tokens = ['a', 'b', 'c', 'd', 'e', 'f'];
+		expect(chunkTokens(tokens, 3, 1)).toEqual([
+			['a', 'b', 'c'],
+			['c', 'd', 'e'],
+			['e', 'f']
+		]);
+	});
+
+	it('returns one chunk if tokens length <= max_embedding', () => {
+		expect(chunkTokens(['a', 'b'], 5, 0)).toEqual([['a', 'b']]);
+	});
+});
+
+// TODO
+// I didnt test poolEmbeddings
+// It requires a Tensor type from @huggingface/transformers
+// i could split the function to make it more testable (just pass dim and data instead of the whole tensor)
+// or mock the Tensor type in this test file
