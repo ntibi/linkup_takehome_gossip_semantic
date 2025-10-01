068f159654ce9f8423e611d2cd12197f7b34e7fe
ntibi <ntibi@protonmail.com>
2025-10-01 15:25:41 +0200

    comment source

--- src/crawler/config.ts
+++ src/crawler/config.ts
@@ -14,11 +14,21 @@ export type Config = {
 // or a kv store (redis, etcd, ...) if the workers were running 24/7 and needed dynamic config updates
 export const config: Config = {
 	sources: [
-		{
-			name: "public",
-			url: "https://www.public.fr",
-			sitemap: "/sitemap.xml",
-		},
+		// TODO
+		// i commented this source
+		// because the sitemap is huge (>200k urls)
+		// so it doesnt take too much time
+		// i could have added a max limit to the sitemap parser
+		// or tweaked the concurrency settings to go faster while still avoiding rate limiting
+		// but i preferred to keep things simple and have a source that gets crawled in a reasonable time out of the box
+		// feel free to uncomment and try it out if you want to (it takes ~1min)
+		// and once the sitemap is fully explored, it's persisted anyway
+
+		//{
+		//name: "public",
+		//url: "https://www.public.fr",
+		//sitemap: "/sitemap.xml",
+		//},
 		{
 			name: "vsd",
 			url: "https://vsd.fr",
